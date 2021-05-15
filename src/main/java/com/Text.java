b2467809ff9894fc51b1bd3e87b21c3043c1aa9c
lcb <2279811886@qq.com>
2021-05-15 14:52:30 +0800

    hell-master3

--- src/main/java/com/Text.java
+++ src/main/java/com/Text.java
@@ -4,5 +4,6 @@ public class Text {
     public static void main(String[] args) {
         System.out.println("hello git-dev");
         System.out.println("hello git");
+        System.out.println("hello master");
     }
 }
